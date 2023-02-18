4fbc1217806d61c750e81f6ca171e2f3fef60e91
Williams <williams.rm99@gmail.com>
2023-02-18 19:11:51 -0400

    obtener usuario por jwt

--- middlewares/validar-jwt.js
+++ middlewares/validar-jwt.js
@@ -1,20 +1,35 @@
 const { response, request } = require("express");
 const jwt = require("jsonwebtoken");
+const Usuario = require("../models/usuario");
 
-const validarJWT = (req = request, res = response, next) => {
+const validarJWT = async (req = request, res = response, next) => {
   const token = req.header("x-token");
 
   if (!token) {
     return res.status(401).json({
-      msg: "There is no token in the request",
+      msg: "Invalid token",
     });
   }
 
   try {
     const { uid } = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
-    req.uid = uid;
+    const usuario = await Usuario.findById(uid);
 
+    if (!usuario) {
+      return res.status(401).json({
+        msg: "Invalid token - user don't exist",
+      });
+    }
+
+    // Verificar si el usuario tiene status en true
+    if (!usuario.status) {
+      return res.status(401).json({
+        msg: "Invalid token - status: false",
+      });
+    }
+
+    req.usuario = usuario;
     next();
   } catch (error) {
     console.log(error);
